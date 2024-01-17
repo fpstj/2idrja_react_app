8e1ae6ebd30a01243da4910dbf148ff6b0be21fa
Filip Stojanov <fs@seveniteen.com>
2024-01-17 16:02:10 +0100

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
+        <div className="alert alert-primary" role="alert">
+          <p style={{ display: 'none' }} className="d-block">
+            Bootstrap is now successfully installed 😃
+          </p>
+          <p className="d-none">Bootstrap is not installed if you can see this 😢</p>
+        </div>
       </header>
     </div>
   );
