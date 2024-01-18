e7c9e5bc840387c1900c1e7e737baaa199a0bb48
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-01-18 20:50:34 +0100

    first row of mission,adding  wallpaper

--- src/OurMission.jsx
+++ src/OurMission.jsx
@@ -6,11 +6,65 @@ const OurMisiion = () => {
     <>
       <div className="mission-content d-flex flex-column justify-content-center align-items-center h-75 ps-5">
         {/* 1st row */}
-        <div className="d-flex flex-row bg-primary h-100">
-          <div></div>
+        <div className="d-flex flex-row h-75 py-5">
+          <div className="d-flex flex-column col-6 p-3">
+            <h1>WHAT WE DO</h1>
+            <hr />
+            <p className=" text-start ps-5">
+              At the core of our organization lies a bold and unwavering mission: to pioneer
+              transformative change through relentless innovation and purpose-driven research. We
+              are on a quest to redefine possibilities, pushing the frontiers of knowledge to
+              address complex challenges and shape a future marked by progress. Guided by a
+              commitment to excellence, collaboration, and societal benefit, our mission is to be a
+              catalyst for positive impact. We aspire to lead the way in pioneering breakthroughs,
+              fostering a culture of curiosity, and driving meaningful advancements that resonate
+              globally. Join us on this journey as we strive to make an indelible mark on the world,
+              driven by a mission that transcends boundaries and inspires a legacy of innovation.
+              <br />
+              Shapes a Limitless Future!"
+            </p>
+          </div>
+          <div className="d-flex flex-column col-6 p-3 pe-5 paragraph">
+            We are driven by a profound commitment to catalyzing progress through pioneering
+            research and innovation.Our focal point is a versatile approach aimed at advancing
+            knowledge, fostering groundbreaking discoveries, and pushing the boundaries of
+            possibility. Through strategic initiatives in research and development, technological
+            innovation, and collaborative partnerships with leading institutions and experts, we
+            endeavor to spearhead transformative changes in various fields.Our dedicated team is at
+            the forefront of scientific exploration, working tirelessly to unlock new solutions,
+            technologies, and methodologies that have the potential to reshape industries and
+            improve the overall human experience.Join us in our relentless pursuit of knowledge, as
+            we strive to create a world where innovation thrives and groundbreaking research propels
+            us towards a brighter
+          </div>
         </div>
-        <div className="d-flex flex-row bg-secondary h-100">h2</div>
-        <div className="d-flex flex-row bg-danger h-100">h3</div>
+
+        <div className="d-flex flex-row bg-secondary h-75">
+          <div className="d-flex flex-column col-6 p-3">
+            <h1>Our mission</h1>
+            <hr />
+            <p className="fw-bold fs-5 text-start ps-5">
+              "Unleashing Innovation, Igniting Change: Where Boundless Research
+              <br />
+              Shapes a Limitless Future!"
+            </p>
+          </div>
+          <div className="d-flex flex-column col-6 p-3 pe-5 paragraph">
+            We are driven by a profound commitment to catalyzing progress through pioneering
+            research and innovation.Our focal point is a versatile approach aimed at advancing
+            knowledge, fostering groundbreaking discoveries, and pushing the boundaries of
+            possibility. Through strategic initiatives in research and development, technological
+            innovation, and collaborative partnerships with leading institutions and experts, we
+            endeavor to spearhead transformative changes in various fields.Our dedicated team is at
+            the forefront of scientific exploration, working tirelessly to unlock new solutions,
+            technologies, and methodologies that have the potential to reshape industries and
+            improve the overall human experience.Join us in our relentless pursuit of knowledge, as
+            we strive to create a world where innovation thrives and groundbreaking research propels
+            us towards a brighter
+          </div>
+        </div>
+
+        <div className="d-flex flex-row bg-danger h-75">h3</div>
       </div>
     </>
   );
