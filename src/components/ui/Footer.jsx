6a1a0c9bec1a5f597ea795e11be003fa53e6a163
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-01-29 14:29:28 +0100

    footer styling

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../icons/logo.svg';
+import logo from '../icons/2drijaLogo.png';
 import { Link } from 'react-router-dom';
 import './Footer.css';
 
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <div className="bg-primary text-white p-5 d-flex flex-row justify-content-around">
       {/* 1st row */}
-      <div className=" d-flex flex-column col-2 justify-content-center align-items-center">
+      <div className=" d-flex flex-column col-3 justify-content-center align-items-center p-3">
         <Link className="navbar-brand" to="/">
           <img className="logo" src={logo} alt="logo of 2DRIJA" />
         </Link>
@@ -16,11 +16,28 @@ const Footer = () => {
         <p>All rights reserved</p>
       </div>
       {/* 2nd row */}
-      <div className="d-flex flex-column col-10 justify-content-center align-items-center ">
-        <div className="d-flex flex-row bg-red">red</div>
-        <div className="d-flex flex-row bg-green">green</div>
-        <div className="d-flex flex-row bg-red">red</div>
-        <div className="d-flex flex-row bg-green">green</div>
+      <div className="d-flex flex-column col-9 justify-content-center align-items-center p-3">
+        <div className="d-flex flex-row ">
+          <div className="col-4 justify-content-center align-items-center p-3">
+            <h5 className="pb-3 text-start">Projects</h5>
+            <p className="text-start">Project name1</p>
+            <p className="text-start">Project name2</p>
+            <p className="text-start">Project name3</p>
+            <p className="text-start">Project name4</p>
+          </div>
+          <div className="col-4 justify-content-center align-items-center p-3">
+            <h5 className="pb-3 text-start">Courses</h5>
+            <p className="text-start">Course name1</p>
+            <p className="text-start">Course name2</p>
+            <p className="text-start">Course name3</p>
+            <p className="text-start">Course name4</p>
+          </div>
+          <div className="col-4 justify-content-center align-items-center p-3">
+            <h6 className="pb-4 text-start">Stay up to date</h6>
+            <input type="email" />
+            <p className="pt-3 fs-5 text-start">Contact us </p>
+          </div>
+        </div>
       </div>
     </div>
   );
