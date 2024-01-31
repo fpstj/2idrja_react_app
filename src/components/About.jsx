ec79c1d38ae91548f691bd1b7f1ca8f71af0cd45
Filip Stojanov <fs@seveniteen.com>
2024-01-31 14:04:15 +0100

    About, Footer2, svg for footer form.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,13 @@
+import React from 'react';
+import Footer from './ui/Footer2';
+
 const About = () => {
   return (
     <>
       <div>
         <h1>About us</h1>
       </div>
+      <Footer />
     </>
   );
 };
