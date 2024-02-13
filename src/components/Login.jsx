e5400d71456610560386588571ef07af15e0ad38
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-02-13 19:24:44 +0100

    login form -fix css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,56 @@
 import './Login.css';
+import Footer from './ui/Footer';
+import Navbar from './ui/Navbar';
 
 const Login = () => {
-  return <>Login page</>;
+  return (
+    <>
+      <Navbar className="active text-white" />
+      <div className="background-wallpaper h-100">
+        <div className="container-fluid container ">
+          <div className="signup-form">
+            <form>
+              <h2>Login</h2>
+              <div className="form-group">
+                {/* email */}
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    placeholder="Email"
+                    required="required"
+                  />
+                </div>
+                {/* password */}
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    placeholder="Password"
+                    required="required"
+                  />
+                </div>
+              </div>
+              <div className="form-group">
+                <label className="form-check-label ">
+                  <input type="checkbox" required="required" /> Remember me
+                </label>
+              </div>
+              <div className="form-group">
+                <button type="submit" className="btn btn-info btn-lg btn-block">
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
 };
 
 export default Login;
