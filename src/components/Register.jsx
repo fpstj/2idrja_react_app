4ee5b2b3cf799565c85ea07a872703a3bfb0ecb5
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-02-13 14:36:39 +0100

    css fix

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -7,77 +7,103 @@ const Register = () => {
   return (
     <>
       <Navbar className="active text-white" />
-      <div className="container-fluid container">
-        <div className="signup-form">
-          <form>
-            <h2>Register</h2>
-            <p className="hint-text">Create your account. </p>
-            <div className="form-group">
-              <div className="row">
-                <div className="col">
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="first_name"
-                    placeholder="First Name"
-                    required="required"
-                  />
-                </div>
-                <div className="col">
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="last_name"
-                    placeholder="Last Name"
-                    required="required"
-                  />
+      <form className={'needs-validation'}>
+        <div className="container-fluid container">
+          <div className="signup-form">
+            <form>
+              <h2>Register</h2>
+              <p className="hint-text">Create your account. </p>
+              <div className="form-group">
+                {/* 1 */}
+                <div className="row">
+                  {/* name */}
+                  <div className="col">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="first_name"
+                      placeholder="First Name"
+                      required="required"
+                    />
+                  </div>
+                  {/* lastname */}
+                  <div className="col">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="last_name"
+                      placeholder="Last Name"
+                      required="required"
+                    />
+                  </div>
                 </div>
               </div>
+              {/* country */}
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Country"
+                  name="country"
+                  required
+                />
+              </div>
+              {/* mobile */}
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Mobile"
+                  name="mobile"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  placeholder="Email"
+                  required="required"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  placeholder="Password"
+                  required="required"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control"
+                  name="confirm_password"
+                  placeholder="Confirm Password"
+                  required="required"
+                />
+              </div>
+              <div className="form-group">
+                <label className="form-check-label">
+                  <input type="checkbox" required="required" /> I accept the{' '}
+                  <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a>
+                </label>
+              </div>
+              <div className="form-group">
+                <button type="submit" className="btn btn-secondary btn-lg btn-block">
+                  Register Now
+                </button>
+              </div>
+            </form>
+            <div className="text-center">
+              Already have an account? <a href="#">Sign in</a>
             </div>
-            <div className="form-group">
-              <input
-                type="email"
-                className="form-control"
-                name="email"
-                placeholder="Email"
-                required="required"
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                className="form-control"
-                name="password"
-                placeholder="Password"
-                required="required"
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                className="form-control"
-                name="confirm_password"
-                placeholder="Confirm Password"
-                required="required"
-              />
-            </div>
-            <div className="form-group">
-              <label className="form-check-label">
-                <input type="checkbox" required="required" /> I accept the{' '}
-                <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a>
-              </label>
-            </div>
-            <div className="form-group">
-              <button type="submit" className="btn btn-secondary btn-lg btn-block">
-                Register Now
-              </button>
-            </div>
-          </form>
-          <div className="text-center">
-            Already have an account? <a href="#">Sign in</a>
           </div>
         </div>
-      </div>
+      </form>
+
       <Footer />
     </>
   );
