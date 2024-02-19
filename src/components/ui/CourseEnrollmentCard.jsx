5fa4277f9d77142cc738ad48bc5cb266b84039b5
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-02-19 17:53:12 +0100

    text-fix

--- src/components/ui/CourseEnrollmentCard.jsx
+++ src/components/ui/CourseEnrollmentCard.jsx
@@ -7,13 +7,13 @@ const CourseEnrollmentCard = () => {
         <div className="enrollment-card  d-flex justify-content-center align-items-center ">
           <div className="card-background ">
             <div className="card-body justify-content-center align-items-center ">
-              <h4 className="card-title text-center p-5">Title</h4>
-              <p className="card-text">Text</p>
-              <p className="card-text">Text</p>
-              <p className="card-text">Text</p>
-              <p className="card-text">Text</p>
+              <h4 className="card-title text-center text-white p-5">Title</h4>
+              <p className="card-text text-white">Text</p>
+              <p className="card-text text-white">Text</p>
+              <p className="card-text text-white">Text</p>
+              <p className="card-text text-white">Text</p>
               <div className="pt-5">
-                <button type="button" className="btn text-white btn-custom">
+                <button type="button" className="btn text-white btn-custom w-75">
                   Enroll
                 </button>
               </div>
