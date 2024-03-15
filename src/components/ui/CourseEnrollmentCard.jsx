1f880c091dfd36394d9a98b7a16f174f154a18fc
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-03-15 02:58:00 +0100

    cards update- fix zoom transition

--- src/components/ui/CourseEnrollmentCard.jsx
+++ src/components/ui/CourseEnrollmentCard.jsx
@@ -8,15 +8,15 @@ const CourseEnrollmentCard = (props) => {
         <div className="enrollment-card d-flex justify-content-center align-items-center p-5">
           <div className="card-background">
             <img className="card-wallpaper h-100" src={props.image} alt="Thumbnail" />
-            <div className="card-body justify-content-center align-items-center pt-5">
+            <div className="card-body justify-content-center align-items-center pt-3">
               <h6 className="card-title text-center text-primary fs-3 fw-bold pb-2">
                 {props.title}
               </h6>
               <p className="card-text text-white">{props.duration}</p>
               <p className="card-text text-dark fs-3 fw-bold">{props.price}</p>
               <p className="card-text text-white">{props.places}</p>
-              <div className="pt-3">
-                <button type="button" className="btn text-light btn-custom w-75">
+              <div className="pt-4">
+                <button type="button" className="btn text-dark btn-custom w-75">
                   Enroll now
                 </button>
               </div>
