b85a28a35dde3dbef94e222218b338ed073052bc
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-03-21 23:57:38 +0100

    fix navbar

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -28,90 +28,93 @@ const Navbar = (props) => {
           <div
             className="collapse navbar-collapse justify-content-center align-items-center"
             id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link text-white" aria-current="page" to="/">
-                  Home
-                </Link>
-              </li>
+            <div className="ms-auto">
+              <ul className="navbar-nav me-auto mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link text-white" aria-current="page" to="/">
+                    Home
+                  </Link>
+                </li>
 
-              <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle text-white"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false">
-                  Projects
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to="/projectscoding">
-                      Coding projects
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="/projectsresearch">
-                      Research projects
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle text-white"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false">
-                  Courses
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to="/coursesfrontend">
-                      Front-end
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="/coursesreact">
-                      React
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="/coursesuiux">
-                      UI/UX
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/about">
-                  About
-                </Link>
-              </li>
-            </ul>
-
-            <ul className="navbar-nav  mb-lg-0 user-icon">
-              <li className="nav-item dropdown ">
-                <Link
-                  className="nav-link dropdown-toggle text-white"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false">
-                  <img src={userLogo} />
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to="/register">
-                      Register
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="/login">
-                      Login
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-            </ul>
+                <li className="nav-item dropdown">
+                  <Link
+                    className="nav-link dropdown-toggle text-white"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Projects
+                  </Link>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <Link className="dropdown-item" to="/projectscoding">
+                        Coding projects
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/projectsresearch">
+                        Research projects
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item dropdown">
+                  <Link
+                    className="nav-link dropdown-toggle text-white"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Courses
+                  </Link>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <Link className="dropdown-item" to="/coursesfrontend">
+                        Front-end
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/coursesreact">
+                        React
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/coursesuiux">
+                        UI/UX
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-white" to="/about">
+                    About
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="ms-auto">
+              <ul className="navbar-nav  mb-lg-0 user-icon">
+                <li className="nav-item dropdown ">
+                  <Link
+                    className="nav-link dropdown-toggle text-white"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    <img src={userLogo} />
+                  </Link>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <Link className="dropdown-item" to="/register">
+                        Register
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/login">
+                        Login
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
 
             {/* <ul className="navbar-nav me-5  mb-lg-0 ">
               <li className="nav-item">
