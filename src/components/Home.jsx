18eb72fa5b3e2e9a17466ccc756a87193b07e833
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-04-21 22:39:10 +0200

    responsivness of  navbar, hero , testimonials, cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,11 @@ import './Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './ui/Navbar';
 import Hero from './ui/Hero';
-import OurMisiion from './ui/OurMission';
-import Footer from './ui/Footer';
-// import CourseEnrollmentCard from './ui/CourseEnrollmentCard';
+// import OurMisiion from './ui/OurMission';
 import Testimonials from './ui/Testimonials';
+// import Footer from './ui/Footer';
 import CourseCard from './ui/CourseCard';
+// import CourseEnrollmentCard from './ui/CourseEnrollmentCard';
 
 function Home() {
   return (
@@ -17,68 +17,53 @@ function Home() {
         <Navbar />
         <Hero />
       </div>
-      <div className="d-flex flex-column">
-        <OurMisiion />
-      </div>
+      <div className="d-flex flex-column">{/* <OurMisiion /> */}</div>
       <div className="d-flex flex-column">
         <Testimonials />
       </div>
 
       <div
-        className="justify-content-center"
-        style={{ display: 'flex', flexWrap: 'wrap', gap: '30px' }}>
-        <CourseCard
-          image="/images/html-wallpaper.jpg"
-          title="HTML Course"
-          duration="01 January - 01 February"
-          discount="120$"
-          price="75$"
-          places="2 places left"
-        />
-
-        <CourseCard
-          image="./images/html-css-js-wallpapers.jpg"
-          title="Frontend course"
-          duration="01 March - 15 April"
-          discount="150$"
-          price="99$"
-          places="7 places left"
-        />
-
-        <CourseCard
-          image="./images/html-css-js-wallpapers.jpg"
-          title="Backend course"
-          duration="01 March - 15 April"
-          discount="200$"
-          price="150$"
-          places="7 places left"
-        />
+        className="course-list-container"
+        // className=" d-flex flex-row justify-content-center align-items-center"
+        // style={{ display: 'flex', flexWrap: 'wrap', gap: '30px' }}
+      >
+        <div className="course-list-wrapper">
+          <CourseCard
+            image="/images/html-wallpaper.jpg"
+            title="HTML Course"
+            duration="01 January - 01 February"
+            discount="120$"
+            price="75$"
+            places="2 places left"
+          />
+        </div>
+        <div className="course-list-wrapper">
+          <CourseCard
+            image="./images/html-css-js-wallpapers.jpg"
+            title="Frontend course"
+            duration="01 March - 15 April"
+            discount="150$"
+            price="99$"
+            places="7 places left"
+          />
+        </div>
+        <div className="course-list-wrapper">
+          <CourseCard
+            image="./images/c-wallpaper.webp"
+            title="C#"
+            duration="01 September - 31 October"
+            discount="200$"
+            price="150$"
+            places="7 places left"
+          />
+        </div>
       </div>
 
-      <div>
+      {/* <div>
         <Footer />
-      </div>
+      </div> */}
     </>
   );
 }
 
 export default Home;
-
-{
-  /* <CourseEnrollmentCard
-          image="./images/html-css-js-wallpapers.jpg"
-          title="Frontend course"
-          duration="01 March - 15 April"
-          price="199$"
-          places="7 places left"
-        />
-
-        <CourseEnrollmentCard
-          image="./images/jswallpaper.jpg"
-          title="JavaScript"
-          duration="01 May - 30 May"
-          discount="150$"
-          price="89$"
-          places="1 place left"
-        /> */
-}
