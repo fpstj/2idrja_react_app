67205bf586d980b5a24f6745d0060dd73ffa55cf
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-05-17 01:18:37 +0200

    fix toggle button to white

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -22,8 +22,9 @@ const Navbar = (props) => {
             data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon "></span>
+            aria-label="Toggle navigation"
+            data-bs-theme="dark">
+            <span className="navbar-toggler-icon"></span>
           </button>
           <div
             className="collapse navbar-collapse justify-content-end align-items-center "
